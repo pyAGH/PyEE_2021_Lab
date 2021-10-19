93d4c7b3b08ff2abe3e27e5ffa59b5e64bc920fd
Qeavah1 <73530030+Qeavah1@users.noreply.github.com>
2021-10-19 14:27:55 +0200

    bin

--- JupyterPyEE/main.py
+++
@@ -1,9 +0,0 @@
-import string, sys
-def ispangram(str1, alphabet=string.ascii_lowercase):
-    alphaset = set(alphabet)
-    print(str1.lower())
-    
-    return alphaset <= set(str1.lower())
-
-
-print (ispangram('The quick brown fox jumps over the lazy dog'))
